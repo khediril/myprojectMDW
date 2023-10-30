cb0b66e11e35df5ea3d7937e201d439be2dfbfe7
khediril <khediril@yahoo.fr>
2023-10-30 08:43:49 +0100

    crud

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    #[Route('/', name: 'app_product')]
     //public function list(EntityManagerInterface $entityManager): Response
     public function list(ProductRepository $repos): Response
     {
@@ -24,4 +24,66 @@ class ProductController extends AbstractController
             'products' => $products,
         ]);
     }
+    #[Route('/{id}', name: 'app_product_show')]
+    //public function list(EntityManagerInterface $entityManager): Response
+    public function show(ProductRepository $repos,$id): Response
+    {
+       // $repos = $entityManager->getRepository(Product::class);
+        
+        $product = $repos->find($id);
+       // dd($product);
+      /*  if (!$product) {
+            throw $this->createNotFoundException(
+                'Id incorrect :'.$id
+            );
+            
+        }*/
+        
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+    #[Route('/new/{name}/{price}', name: 'app_product_new')]
+    //public function list(EntityManagerInterface $entityManager): Response
+    public function add($name,$price,EntityManagerInterface $entityManager): Response
+    {
+       $product = new Product();
+       $product->setName($name);
+       $product->setPrice($price);
+       $product->setDescription("c'est une promo...");
+       $entityManager->persist($product);
+       $entityManager->flush();
+
+        // $repos = $entityManager->getRepository(Product::class);
+        
+       
+        
+        
+        return $this->render('product/add.html.twig', [
+            
+        ]);
+    }
+    #[Route('/delete/{id}', name: 'app_product_delete')]
+    //public function list(EntityManagerInterface $entityManager): Response
+    public function delete(ProductRepository $repos,$id,EntityManagerInterface $entityManager): Response
+    {
+       // $repos = $entityManager->getRepository(Product::class);
+        
+        $product = $repos->find($id);
+       // dd($product);
+      /*  if (!$product) {
+            throw $this->createNotFoundException(
+                'Id incorrect :'.$id
+            );
+            
+        }*/
+        $entityManager->remove($product);
+        $entityManager->flush();
+        return $this->redirectToRoute("app_product");
+        
+      /*  return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);*/
+    }
+    
 }
